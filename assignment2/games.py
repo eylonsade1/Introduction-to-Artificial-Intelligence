65db7d09b0f57538b8278710d59335bb0d178c48
eylonsade1 <63980535+eylonsade1@users.noreply.github.com>
2022-12-24 17:43:45 +0200

    Removed unwanted functions

--- assignment2/games.py
+++ assignment2/games.py
@@ -1,44 +1,133 @@
 import numpy as np
+from Graph import Graph
+from Agent import Agent
+# def alpha_beta_search(state, game):
+#     """Search game to determine best action; use alpha-beta pruning.
+#     As in [Figure 5.7], this version searches all the way to the leaves."""
+#
+#     player = game.to_move(state)
+#
+#     # Functions used by alpha_beta
+#     def max_value(state, alpha, beta):
+#         if game.terminal_test(state):
+#             return game.utility(state, player)
+#         v = -np.inf
+#         for a in game.actions(state):
+#             v = max(v, min_value(game.result(state, a), alpha, beta))
+#             if v >= beta:
+#                 return v
+#             alpha = max(alpha, v)
+#         return v
+#
+#     def min_value(state, alpha, beta):
+#         if game.terminal_test(state):
+#             return game.utility(state, player)
+#         v = np.inf
+#         for a in game.actions(state):
+#             v = min(v, max_value(game.result(state, a), alpha, beta))
+#             if v <= alpha:
+#                 return v
+#             beta = min(beta, v)
+#         return v
+#
+#     # Body of alpha_beta_search:
+#     best_score = -np.inf
+#     beta = np.inf
+#     best_action = None
+#     for a in game.actions(state):
+#         v = min_value(game.result(state, a), best_score, beta)
+#         if v > best_score:
+#             best_score = v
+#             best_action = a
+#     return best_action
 
-def alpha_beta_search(state, game):
-    """Search game to determine best action; use alpha-beta pruning.
-    As in [Figure 5.7], this version searches all the way to the leaves."""
 
-    player = game.to_move(state)
+def min_value(self, state: s.State, num_of_plys, WORLD: Graph, agent: Agent):
+    if state.terminal_state(num_of_plys):
+        return state.evaluate()
+    best_value = None
+    for next_state in state.successor("MIN", WORLD):
+        next_state_max_value = self.max_value(next_state, num_of_plys + 1, WORLD)
+        if best_value is None:
+            best_value = next_state_max_value
+        if agent.type == 1:
+            best_value = agent.other_agent.comparator(best_value, next_state_max_value)
+        else:
+            best_value = agent.comparator(best_value, next_state_max_value)
+    return best_value
 
-    # Functions used by alpha_beta
-    def max_value(state, alpha, beta):
-        if game.terminal_test(state):
-            return game.utility(state, player)
-        v = -np.inf
-        for a in game.actions(state):
-            v = max(v, min_value(game.result(state, a), alpha, beta))
-            if v >= beta:
-                return v
-            alpha = max(alpha, v)
-        return v
 
-    def min_value(state, alpha, beta):
-        if game.terminal_test(state):
-            return game.utility(state, player)
-        v = np.inf
-        for a in game.actions(state):
-            v = min(v, max_value(game.result(state, a), alpha, beta))
-            if v <= alpha:
-                return v
-            beta = min(beta, v)
-        return v
+def max_value(self, state, num_of_plys, WORLD: Graph, agent: Agent):
+    if state.terminal_state(num_of_plys):
+        return state.evaluate()
+    best_value = None
+    for next_state in state.successor("MAX", WORLD):
+        next_state_min_value = self.min_value(next_state, num_of_plys + 1, WORLD, agent)
+        if best_value is None:
+            best_value = next_state_min_value
+        if agent.type == 1:
+            best_value = agent.comparator(best_value, next_state_min_value)
+        else:
+            best_value = agent.other_agent.comparator(best_value, next_state_min_value)
+    return best_value
 
-    # Body of alpha_beta_search:
-    best_score = -np.inf
-    beta = np.inf
-    best_action = None
-    for a in game.actions(state):
-        v = min_value(game.result(state, a), best_score, beta)
-        if v > best_score:
-            best_score = v
-            best_action = a
-    return best_action
+
+def maxValueAlphaBeta(self, state, num_of_plys, graph: Graph, alpha, beta):
+    if state.terminal_state(num_of_plys):
+        # def evaluate_alpha_beta(self): return self.max_agent_score - self.min_agent_score
+        return state.evaluate_alpha_beta()
+    v = float('-inf')
+    for next_state in state.successor("MAX", graph):
+        v = max(v, self.minValueAlphaBeta(next_state, num_of_plys + 1, graph, alpha, beta))
+        if v >= beta:
+            return v
+        alpha = max(alpha, v)
+    return v
+
+
+def minValueAlphaBeta(self, state: s.State, num_of_plys, graph: Graph, alpha, beta):
+    if state.terminal_state(num_of_plys):
+        return state.evaluate_alpha_beta()
+    v = float('inf')
+    for next_state in state.successor("MIN", graph):
+        v = min(v, self.maxValueAlphaBeta(next_state, num_of_plys + 1, graph, alpha, beta))
+        if v <= alpha:
+            return v
+        beta = min(beta, v)
+    return v
+
+
+def minimaxAlphaBeta(self, state:s.State, graph:Graph):
+    best_edge = None
+    num_of_plys = 0
+    v = float('-inf')
+    alpha = float('-inf')
+    beta = float('inf')
+    for next_state in state.successor("MAX", graph):
+        value_of_new_state = self.minValueAlphaBeta(next_state, num_of_plys + 1, graph, alpha, beta)
+        current_edge = graph.get_edge(next_state.max_agent_current_location.prev,next_state.max_agent_current_location.successor)
+        if v < value_of_new_state:
+            v = value_of_new_state
+            best_edge = current_edge
+        alpha = max(v, alpha)
+    return [best_edge[0], best_edge[1], best_edge[2]]
+
+
+def maximinAlphaBeta(self, state: s.State, graph: Graph):
+    best_edge = None
+    num_of_plys = 0
+    v = float('inf')
+    alpha = float('-inf')
+    beta = float('inf')
+    for next_state in state.successor("MIN", graph):
+        value_of_new_state = self.maxValueAlphaBeta(next_state, num_of_plys + 1, graph, alpha, beta)
+        current_edge = graph.get_edge(next_state.min_agent_current_location.prev, next_state.min_agent_current_location.successor)
+        if v > value_of_new_state:
+            v = value_of_new_state
+            best_edge = current_edge
+        beta = min(v, beta)
+    return [best_edge[0], best_edge[1], best_edge[2]]
+
 
 def query_player(game, state):
     """Make a move by querying standard input."""
@@ -57,8 +146,8 @@ def query_player(game, state):
         print('no legal moves: passing turn to next player')
     return move
 
-def alpha_beta_player(game, state):
-    return alpha_beta_search(state, game)
+# def alpha_beta_player(game, state):
+#     return alpha_beta_search(state, game)
 
 class Game:
     """A game is similar to a problem, but it has a utility for each
@@ -107,45 +196,75 @@ class Game:
                     self.display(state)
                     return self.utility(state, self.to_move(self.initial))
 
-def alpha_beta_cutoff_search(state, game, d=4, cutoff_test=None, eval_fn=None):
-    """Search game to determine best action; use alpha-beta pruning.
-    This version cuts off search and uses an evaluation function."""
 
-    player = game.to_move(state)
+    def alpha_beta_cutoff_search(state, game, d=4, cutoff_test=None, eval_fn=None):
+        """Search game to determine best action; use alpha-beta pruning.
+        This version cuts off search and uses an evaluation function."""
 
-    # Functions used by alpha_beta
-    def max_value(state, alpha, beta, depth):
-        if cutoff_test(state, depth):
-            return eval_fn(state)
-        v = -np.inf
+        player = game.to_move(state)
+
+        # Functions used by alpha_beta
+        def max_value(state, alpha, beta, depth):
+            if cutoff_test(state, depth):
+                return eval_fn(state)
+            v = -np.inf
+            for a in game.actions(state):
+                v = max(v, min_value(game.result(state, a), alpha, beta, depth + 1))
+                if v >= beta:
+                    return v
+                alpha = max(alpha, v)
+            return v
+
+        def min_value(state, alpha, beta, depth):
+            if cutoff_test(state, depth):
+                return eval_fn(state)
+            v = np.inf
+            for a in game.actions(state):
+                v = min(v, max_value(game.result(state, a), alpha, beta, depth + 1))
+                if v <= alpha:
+                    return v
+                beta = min(beta, v)
+            return v
+
+        # Body of alpha_beta_cutoff_search starts here:
+        # The default test cuts off at depth d or at a terminal state
+        cutoff_test = (cutoff_test or (lambda state, depth: depth > d or game.terminal_test(state)))
+        eval_fn = eval_fn or (lambda state: game.utility(state, player))
+        best_score = -np.inf
+        beta = np.inf
+        best_action = None
         for a in game.actions(state):
-            v = max(v, min_value(game.result(state, a), alpha, beta, depth + 1))
-            if v >= beta:
-                return v
-            alpha = max(alpha, v)
-        return v
+            v = min_value(game.result(state, a), best_score, beta, 1)
+            if v > best_score:
+                best_score = v
+                best_action = a
+        return best_action
 
-    def min_value(state, alpha, beta, depth):
-        if cutoff_test(state, depth):
-            return eval_fn(state)
-        v = np.inf
-        for a in game.actions(state):
-            v = min(v, max_value(game.result(state, a), alpha, beta, depth + 1))
-            if v <= alpha:
-                return v
-            beta = min(beta, v)
-        return v
 
-    # Body of alpha_beta_cutoff_search starts here:
-    # The default test cuts off at depth d or at a terminal state
-    cutoff_test = (cutoff_test or (lambda state, depth: depth > d or game.terminal_test(state)))
-    eval_fn = eval_fn or (lambda state: game.utility(state, player))
-    best_score = -np.inf
-    beta = np.inf
-    best_action = None
-    for a in game.actions(state):
-        v = min_value(game.result(state, a), best_score, beta, 1)
-        if v > best_score:
-            best_score = v
-            best_action = a
-    return best_action
+class UtilityFuncs:
+    def maxSemiCooperative(self, bestVal, newState):
+        if bestVal[0] < newState[0]:
+            return newState
+        if bestVal[0] == newState[0]:
+            if bestVal[1] < newState[1]:
+                return newState
+        return bestVal
+
+    def minSemiCooperative(self, bestVal, newState):
+        if bestVal[1] < newState[1]:
+            return newState
+        if bestVal[1] == newState[1]:
+            if bestVal[0] < newState[0]:
+                return newState
+        return bestVal
+
+    def fullyCooperative(self, bestVal, newState):
+        bestValSum = bestVal[0] + bestVal[1]
+        newVal = newState[0] + newState[1]
+        bestValMovements = bestVal[2]
+        newStateMovements = newState[2]
+        if bestValSum < newVal:
+            return newState
+        elif bestValSum == newVal and bestValMovements > newStateMovements:
+            return newState
+        return bestVal
